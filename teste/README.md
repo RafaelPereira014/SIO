fe023b6bf0b5cdae49887664cfa21a6455e871ff
RafaelPereira014 <rafaelbotelhopereira@ua.pt>
2022-02-02 15:54:38 +0000

    test

---
+++ teste/README.md
@@ -0,0 +1,24 @@
+# Authors
+->Daniel Frank (n°96127)
+
+->Denis Yamunaque (nº)
+
+->Diogo Aguiar (nº81020)
+
+->Rafael Pereira (nº98354)
+
+# Project Description
+The project developed was a school system, in which students have a profile where they can consult their grades in a certain subject, send questions to a teacher and pay the tuition fee for the institution. In addition, there is also the page for the teacher of each subject, where they can consult the grades of each class (where, only they, has access to the grade of all students) and check the questions sent.
+The app was developed using, in the frontend HTML5 and CSS, and in the beckend NodeJS and the Express library.
+
+# Vulnerabilities
+->CWE-89:SQL Injection
+
+->CWE-79:Cross Site Scripting
+
+->CWE-434: Unrestricted Upload of File with Dangerous Type
+
+->CWE-20: Improper Input Validation
+
+->CWE-2087: Improper Authentication
+
---
+++ teste/app/sql.js
@@ -0,0 +1,34 @@
+var sqlite3 = require('sqlite3').verbose()
+var db = new sqlite3.Database('users.db')
+
+db.serialize(function () {
+
+  // Questions for teachers table
+  db.run("CREATE TABLE questions (username INT, content TEXT, teacher TEXT)");
+
+  //TEACHERS TABLE
+  db.run("CREATE TABLE teacher (username TEXT, password TEXT,name TEXT,subject TEXT,userid INT)");
+
+  db.run("INSERT INTO teacher (username,password,name, subject,userid) VALUES ('albert_ast','1234','Alberto Astuto','Física','1')");
+  db.run("INSERT INTO teacher (username,password,name, subject,userid) VALUES ('jjtom','4567','João José Tomate','Química','2')");
+  db.run("INSERT INTO teacher (username,password,name, subject,userid) VALUES ('rendesca','8910','Renan Descalço','Matemática','3')");
+  
+
+  //STUDENTS TABLE
+
+  db.run("CREATE TABLE student (username TEXT, password TEXT,name TEXT,number TEXT, classroom TEXT, gradeM INT, gradeP INT, gradeC INT, userid INT, saldo INT)");
+
+  db.run("INSERT INTO student (username,password, name, number,classroom, gradeM, gradeP, gradeC, userid,saldo) VALUES ('danielbueeno','1112','Daniel Bueno', '666','10', 20, 20, 20, 4, 1000)");
+  db.run("INSERT INTO student (username,password, name, number,classroom, gradeM, gradeP, gradeC, userid,saldo) VALUES ('rafaelpereira','1314','Rafael Pereira', '777','11', 20, 20, 10, 5, 1000)");
+  db.run("INSERT INTO student (username,password, name, number,classroom, gradeM, gradeP, gradeC, userid,saldo) VALUES ('denisyamunaque','1415','Denis Yamunaque', '888','12', 18, 15, 10, 6, 1000)");
+
+  //Grades TABLE
+
+  db.run("CREATE TABLE pautas (number TEXT, gradeMath INT, gradePhys INT, gradeChe INT)");
+  db.run("INSERT INTO pautas (number, gradeMath, gradePhys, gradeChe) VALUES ('666','20', '15','10')");
+  db.run("INSERT INTO pautas (number, gradeMath, gradePhys, gradeChe) VALUES ('777','10', '9','18')");
+  db.run("INSERT INTO pautas (number, gradeMath, gradePhys, gradeChe) VALUES ('888','5', '2','10')");
+
+})
+
+db.close()
